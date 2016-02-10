176cfd5266fb90102f9753bda54149e4776ae057
Monica Dinculescu <noms@google.com>
2016-02-09 21:38:49 -0800

    github oauth is working YAY

---
+++ gp.sh
@@ -0,0 +1,26 @@
+org=${1:-"notwaldorf"}
+repo=${2:-"github-triage-dashboard"}
+
+polybuild index.html
+echo js:   `gzip -c index.build.js | wc -c`
+echo html: `gzip -c index.build.html | wc -c`
+
+rm -rf deploy
+git clone -b gh-pages --single-branch git@github.com:$org/$repo.git deploy
+
+mv index.build.html deploy/index.html
+mv index.build.js deploy
+cp sw.js deploy
+polybuild index-debug.html
+mv index-debug.build.html deploy/index-debug.html
+mv index-debug.build.js deploy
+
+cp callback.html deploy
+rm -rf deploy/resources
+cp -rf resources deploy
+
+pushd deploy >/dev/null
+git add -A .
+git commit -am 'update gh-pages'
+git push -u origin gh-pages:gh-pages
+popd >/dev/null
